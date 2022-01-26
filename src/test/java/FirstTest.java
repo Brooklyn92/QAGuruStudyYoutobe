6782bf30ee840d39e1c76b9f0a67d0b2ac558ebb
Brooklyn92 <Tubanar173russia>
2022-01-26 18:01:08 +0400

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    @Test
+    public void firstTest() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.tvnet.lv/");
+        WebElement accCookie = driver.findElement(By.xpath("//button[@mode='primary']"));
+        accCookie.click();
+    }
+}
