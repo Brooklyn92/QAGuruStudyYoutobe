22ef7c1a5a6ad326e15d77ffe1cd2a4a2344ed51
Brooklyn92 <Tubanar173russia>
2022-03-21 16:58:07 +0400

    study lesson7 final

--- src/test/java/tvnet/TvNetTest.java
+++ src/test/java/tvnet/TvNetTest.java
@@ -1,26 +1,53 @@
 package tvnet;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class TvNetTest {
     private final String HOME_PAGE_URL = "https://www.tvnet.lv/";
     private final By ACCEPT_COOKIES = By.xpath("//button[@mode='primary']");
+    private final By ARTICLE_TITLE = By.xpath(".//span[@itemprop='headline name']");
+    private WebDriver driver;
+
     @Test
     public void titleCheck() {
         System.setProperty("webdriver.chrome.driver", "C://webdrivers//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));  // в параметрах указывается объект браузерного окна и параметр объекта продолжительности ожидания.
+                                                                                // Данное ожидаение является не ЯВНЫМ
+
+        //================ВХОД НА ГЛАВНУЮ СТРАНИЦУ И ПРИНЯТИЕ КУКОВ=====================================
         driver.get(HOME_PAGE_URL);
-        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES));
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES));// в методе until( ждать ДО) указываем в параметраъ объект ExpectedConditions(ожидаемые условия)
         driver.findElement(ACCEPT_COOKIES).click();
+
+        //================ПОЛУЧЕНИЕ ТЕКСТА ЗАГОЛОВКА==============================================================
+        WebElement firstTitle = driver.findElement(ARTICLE_TITLE);
+        System.out.println(firstTitle.getText());
+
+        //================ПОЛУЧЕНИЕ ТЕКСТА ЗАГОЛОВКА ЧЕРЕЗ КОЛЛЕКЦИЮ LIST И МЕТОДА findElements====================
+        List<WebElement> titles = driver.findElements(ARTICLE_TITLE);
+        System.out.println(titles.get(0).getText());
+        System.out.println(titles.get(0).getText().equals(firstTitle.getText()));
+
+        //================ПОЛУЧЕНИЕ И ПЕЧАТЬ ВСЕХ НАЙДЕННЫЙХ ЗАГОЛОВКОВ=============================================
+        for (WebElement title:titles) {
+            System.out.println(title.getText());
+        }
+    }
+
+    @AfterEach // аннтотация, которая помечает,что метод запускается после отработки кажого метода.
+    public void closeBrowser() {
+        driver.close();
     }
 }
