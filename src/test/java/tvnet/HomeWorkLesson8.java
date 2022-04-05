a7513d0b3f433dfcb7837891fd018e1b60865ba7
Brooklyn92 <Tubanar173russia>
2022-04-05 17:56:02 +0400

    study lesson8 pt2

--- src/test/java/tvnet/HomeWorkLesson8.java
+++ src/test/java/tvnet/HomeWorkLesson8.java
@@ -1,6 +1,4 @@
 package tvnet;
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -10,10 +8,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 import java.util.List;
 
-public class TvNetTest {
+public class HomeWorkLesson8 {
     private final String HOME_PAGE_URL = "https://www.tvnet.lv/";
     private final By ACCEPT_COOKIES = By.xpath("//button[@mode='primary']");
     private final By ARTICLE_TITLE = By.xpath(".//span[@itemprop='headline name']");
@@ -27,8 +26,8 @@ public class TvNetTest {
         driver = new ChromeDriver();
         driver.manage().window().setPosition(new Point(2000,0));
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));  // в параметрах указывается объект браузерного окна и параметр объекта продолжительности ожидания.
-                                                                                // Данное ожидаение является не ЯВНЫМ
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  // в параметрах указывается объект браузерного окна и параметр объекта продолжительности ожидания.
+        // Данное ожидаение является не ЯВНЫМ
 
         //================ВХОД НА ГЛАВНУЮ СТРАНИЦУ И ПРИНЯТИЕ КУКОВ=====================================
         driver.get(HOME_PAGE_URL);
@@ -47,43 +46,6 @@ public class TvNetTest {
 
         WebElement firstTitle = driver.findElement(ARTICLE_PAGE_TITLE);
         Assertions.assertEquals(GIVEN_TITLE, firstTitle.getText(), "Wrong title"); //GIVEN_TITLE-ожидаемый результат, firstTitle - фактический результат
-
-
-       /*
-        //================ПОЛУЧЕНИЕ ТЕКСТА ЗАГОЛОВКА  и ПОЛУЧЕНИЕ ТЕКСТА ЗАГОЛОВКА ЧЕРЕЗ КОЛЛЕКЦИЮ LIST И МЕТОДА findElements======================================
-        WebElement firstTitle = driver.findElement(ARTICLE_TITLE);
-        System.out.println(firstTitle.getText());
-        List<WebElement> titles = driver.findElements(ARTICLE_TITLE);
-
-        List<WebElement> titles = driver.findElements(ARTICLE_TITLE);
-        System.out.println(titles.get(0).getText());
-        System.out.println(titles.get(0).getText().equals(firstTitle.getText()));
-
-
-
-        //================ПОЛУЧЕНИЕ И ПЕЧАТЬ ВСЕХ НАЙДЕННЫЙХ ЗАГОЛОВКОВ=============================================
-        for (WebElement title:titles) {
-            System.out.println(title.getText());
-        }
-
-
-        //================================FOR====================================================
-//        for (int i = 0; i<titles.size(); i++ ) {
-//            System.out.println(i+1 + ": " + titles.get(i).getText());
-//        }
-
-        //==========================================WHILE================================================
-//        int i = 0;
-//        while (i<titles.size()) {
-//            System.out.println(i+1 + ": " + titles.get(i).getText());
-//            i++;
-//        }
-
-        */
     }
 
-    @AfterEach // аннтотация, которая помечает,что метод запускается после отработки кажого метода.
-    public void closeBrowser() {
-        //driver.close();
-    }
 }
